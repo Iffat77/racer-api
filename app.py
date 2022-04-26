7ca4f4d9ffbbe2682bdefdab657f194350e8cf86
Iffat Hossain <iffathossain@Iffats-MacBook-Air.local>
2022-04-26 11:39:50 -0400

    update git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/Iffat77/racer-api.git git push -u origin main

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-from array import ArrayType
-from multiprocessing.dummy import Array
-from unicodedata import name
 from flask import Flask, request, jsonify
 from peewee import *
 from playhouse.shortcuts import model_to_dict, dict_to_model
